b6c6e3a10774b75a0f0d7a9b69a78f1373533eae
José <ncode.fun@gmail.com>
2023-02-16 17:46:37 -0500

    Moved presentation to own #page.

--- public/root/app.js
+++ public/root/app.js
@@ -27,17 +27,11 @@ class App extends VS {
 
 				<h2><em>✦ Zero friction ⇢ Pure focus ! ✦</em></h2>
 				<p>
-					Vision stage is a minimalist & intuitive Web component & Web app library, using lit-html templates and reactive properties – all with vanilla JS/HTML. No build step required.
-
-				<h2>KISS ❤</h2>
+					Vision stage is a minimalist & intuitive Web component & Web app library, using lit-html templates <span class='nowrap'>and reactive properties</span> – all with vanilla JS/HTML.
+					<span class='nowrap'>No build step required.</span>
 				<p>
-					Vision Stage provides – I beleive – the best developer experience out there… Its simplicity frees you from wasteful diversions with the concerns, confusion and frustrations of working with non-standard syntax, concepts and workflow / tooling that other, complex libraries and frameworks force you to deal with.
-				<p>
-					Such true simplicity means you can **stay in the creative flow**, and **focus on what matter – *your* app !** In my book, **that** is truly agile, and future-proof development…
-				<p>
-					Forget about ultra-optimizing everything; simplicity is already, by nature, lightweight and fast. Quite fast enough in fact, compared to so many small and medium apps using way overkill frameworks…
-				<p>
-					I hope you find the same joy working to build your ideas with Vision Stage as the joy I had and still have developing it ! ✌
+					<a href='#Presentation'>Presentation</a>
+				</p>
 			</div>
 
 			<div lang='fr' class='intro'>
@@ -46,7 +40,30 @@ class App extends VS {
 				<h2><em>✦ Zéro friction ⇢ Pure focus ! ✦</em></h2>
 				<p>
 					Vision stage, c'est une librairie minimaliste et intuitive pour bâtir des composantes & des applis Web moderne, utilisant des <i>templates</i> lit-html et des propriétés réactives – tout ça en pure JS/HTML. Pas de <i>build step<i> requit.
+				<p>
+					<a href='#Présentation'>Présentation</a>
+				</p>
 
+			</div>
+		</main>
+	`
+
+	presentation = () => html`
+		<main id='presentation' flow='col top grow' class='text-justify'>
+			<div lang='en'>
+				<h1>Vision <small>✦</small> Stage</h1>
+				<h2>KISS ❤</h2>
+				<p>
+					Vision Stage provides – I beleive – the best developer experience out there… Its simplicity frees you from wasteful diversions with the concerns, confusion and frustrations of working with non-standard syntax, concepts and workflow / tooling that other, complex libraries and frameworks force you to deal with.
+				<p>
+					Such true simplicity means you can <strong>stay in the creative flow</strong>, and <strong>focus on what matter – <em>your</em> app !</strong> In my book, <strong>that</strong> is truly agile, and future-proof development…
+				<p>
+					Forget about ultra-optimizing everything; simplicity is already, by nature, lightweight and fast. Quite fast enough in fact, compared to so many small and medium apps using way overkill frameworks…
+				<p>
+					I hope you find the same joy working to build your ideas with Vision Stage as the joy I had and still have developing it ! ✌
+			</div>
+
+			<div lang='fr'>
 				<h2>KISS ❤</h2>
 				<p>
 					Vision Stage procure – je le crois – la meilleure expérience développeur ici-bas… Sa simplicité nous libère des pertes de temps en diversions avec les préoccupations, la confusion et les frustrations à travailler avec une syntaxe, des concepts et un workflow / outillage non-standard, que les autres librairie et frameworks complexes nous forces à utiliser.
@@ -60,38 +77,26 @@ class App extends VS {
 		</main>
 	`
 
-	motivation = () => html`
-		<main id='motivation' flow='col top grow' class='text-justify'>
-			<h2>Motivation</h2>
-			<div lang='en'>
-
-
-
-				<p>❤️</p>
-			</div>
-
-			<div lang='fr'>
-				<p>
-
-				</p>
-			</div>
-		</main>
-	`
-
 	stage = () => html`
 		<main id='stage' flow='col top grow' class='text-justify'>
 			<h2>The Stage</h2>
+
+			<h3>Why "Vision Stage" ?</h3>
+			<p>
+				1) It's ideal for quickly starting to build your app while in the ideation stage, the "vision" stage, since there's zero friction to slow you down and frustrate you and get you out of your creative flow.
+			<p>
+				2) The app is a special component, a visual stage to frame your vision, kept within specific aspect-ratios; so it's a way to stage and resize your content consistently like with games.
+
 			<p>
 				Vision Stage came about as a framework for developing full page apps / mini-games, where you want your "staged" content to always remain visible and look the same on any screen aspect. Thus the top-level, "App" element <code>&lt;vision-stage></code> acts as a stage:
-			</p>
 			<p>
 				1) It scales the rem value (&lt;html> font-size), therefore the content sized in em/rem units scales to always fit the stage vertically, or horizontally – if we define a <code>portrait</code> aspect-ratio – when the viewport aspect-ratio is under that ratio.
-			</p>
 			<p>
 				2) Optionally, using aspect-ratio boundaries provides a neat way to frame your landscape and portrait layouts by keeping the stage's dimensions within specific aspect-ratios. This prevents the layout space to extend or contract to much, so you don't have to manage how your content displays within extreme or in-between aspect-ratios. You only define a layout for landscape, and maybe allow for a wider space up to a defined <code>landscape_max</code> ratio, and you can also make a layout for portrait, and by default we allow vertical space on very narrow screens to extend without limit, for it would look weird to have empty space above and beyond the stage on phone screens… There's also a <code>portrait_alt</code> ratio that we can define for elements with the class <code>alt-scaling</code>, so their content will still be scaled relative to the stage height down to a lower ratio than portrait, thus content will be bigger than the normal content on narrow screens; this is useful for header and footer items where theres enough room.
+			<p>
+				To see this in action, just make this window very wide and slowly reduce its width til it's very narrow like a phone; you will see all different aspects, which here are:
 			</p>
-			<p>To see this in action, just make this window very wide and slowly reduce its width til it's very narrow like a phone; you will see all different aspects, which here are:
-			</p>
+
 			<ul class='frame text-left'>
 				<li><code>portrait_alt: 0.5</code> <wbr><em>// alternative min horizontal space <wbr>in portrait, for elements with class <code>alt-scaling</code>.</em></li>
 				<li><code>portrait: 0.6</code> <wbr><em>// min horizontal space in portrait.</em></li>
@@ -153,8 +158,8 @@ VS.config = {
 VS.aspects = {
 	// portrait_min: 	.37,	// max vertical space in portrait
 	portrait_alt: .5,			//
-	portrait: 		.6,		// min horizontal space in portrait
-	portrait_max: 	.6,		// max horizontal space in portrait
+	portrait: 		.66,		// min horizontal space in portrait
+	//portrait_max: 	.66,		// max horizontal space in portrait
 	landscape: 		4/3,		// min horizontal space in landscape
 	landscape_max: 1.85,		// max horizontal space in landscape
 	threshold: 		1.2,
@@ -172,8 +177,7 @@ VS.languages = ['en', 'fr']
 
 VS.pages = {
 	'': 					["Home", "Accueil"],
-	//why:				["Why", "Pourquoi"],
-	motivation: 		["Motivation"],
+	presentation: 		["Presentation", "Présentation"],
 	stage:				["The Stage", "Le Stage"],
 	demos:				["Demos", "Démos"],
 	start:				["Start!", "Commencer!"],
